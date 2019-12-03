d1f3321f766953469f3a9d8bdfdd0eda6ef00276
npatel6687 <neethika.patel@gmail.com>
2019-12-03 17:35:20 +0000

    first commit

--- src/test/ChallengingDOM.java
+++ src/test/ChallengingDOM.java
@@ -1,19 +1,16 @@
 package test;
 
 import java.io.IOException;
-import java.util.ArrayList;
+
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.util.Set;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -27,7 +24,7 @@ public class ChallengingDOM extends OpenBrowser{
 	
 	@Test(priority = 0)
 	public  void verifyTitle() throws IOException {
-		String expectedTitle = "The Internet";
+		
 		String actualTitle = driver.getTitle();
 		Assert.assertEquals(actualTitle, expectedTitle);
 		System.out.println("Title: "+actualTitle);
@@ -36,7 +33,6 @@ public class ChallengingDOM extends OpenBrowser{
 
 	@Test(priority = 1)
 	public  void verifyHeading() throws IOException {
-		String expectedHeading = "Challenging DOM";
 		WebElement table = driver.findElement(By.cssSelector("div[class='large-10 columns']"));
 		String actualHeading = table.findElement(By.xpath("//div[@class='example']/h3")).getText();
 		Assert.assertEquals(actualHeading, expectedHeading);
@@ -46,7 +42,6 @@ public class ChallengingDOM extends OpenBrowser{
 	
 	@Test(priority = 2)
 	public  void verifyDescription() throws IOException {
-		String expectedDesc = "The hardest part in automated web testing is finding the best locators (e.g., ones that well named, unique, and unlikely to change). It's more often than not that the application you're testing was not built with this concept in mind. This example demonstrates that with unique IDs, a table with no helpful locators, and a canvas element.";
 		
 		WebElement table = driver.findElement(By.cssSelector("div[class='large-10 columns']"));
 		String actualDesc = table.findElement(By.xpath("//div[@class='example']/p")).getText();
@@ -104,71 +99,102 @@ public  void verifyRedButtonClick() {
 			WebElement canvasElement = driver.findElement(By.cssSelector("div[class='large-10 columns']"));
 			boolean answerDisplayed = canvasElement.findElement(By.xpath("//canvas[@id='canvas']")).isDisplayed();
 			Assert.assertTrue(answerDisplayed);
-			System.out.println("Answer displayed: "+answerDisplayed);
-			
-			//JavascriptExecutor js = (JavascriptExecutor) driver;
-			//String query = "return document.getElementsByTagName(\"script\").canvas;";    
-			//String text=(String) js.executeScript(query);
-			//System.out.println(text);
-			
+			System.out.println("Answer displayed: "+answerDisplayed);	
 	}
-	@Test
+	@Test(priority = 7)
 	public  void verifyTableHeadings() {
 		WebElement table = driver.findElement(By.cssSelector("div[class='large-10 columns']"));
 		WebElement actionTable = table.findElement(By.tagName("table"));
 		int headingsCount = actionTable.findElements(By.tagName("th")).size();
 		System.out.println(headingsCount);
 		List<String> expHeadings = Arrays.asList("Lorem", "Ipsum", "Dolor","Sit","Amet","Diceret","Action"); 
-		for(int i=0,count=1;i<headingsCount;i++,count++){
+		for(int i=0;i<headingsCount;i++){
 				String actualHeading = actionTable.findElements(By.tagName("th")).get(i).getText();
 				Assert.assertEquals(actualHeading, expHeadings.get(i));
 				//System.out.println("expheading: "+expHeadings.get(i));
-				System.out.println(count + ": "+actionTable.findElements(By.tagName("th")).get(i).getText());
+				//System.out.println(count + ": "+actionTable.findElements(By.tagName("th")).get(i).getText());
 				
 		}
 	}
 	
-	@Test
-	public  void verifyRows() {
+	@Test(priority = 8)
+	public  void validateRowData() {
 		WebElement table = driver.findElement(By.cssSelector("div[class='large-10 columns']"));
 		WebElement actionTable = table.findElement(By.tagName("table"));
 		int headingsCount = actionTable.findElements(By.tagName("th")).size();
-		int rowCount = actionTable.findElements(By.tagName("tr")).getSize();
-		System.out.println(headingsCount);
-		List<String> expRow1 = Arrays.asList("Iuvaret0", "Apeirian0	", "Adipisci0","Definiebas0","Consequuntur0","Phaedrum0"); 
-		for(int i=0;i<headingsCount-1;i++){
-				String actualHeading = actionTable.findElements(By.tagName("tr")).get(i).getText();
-				Assert.assertEquals(actualHeading, expHeadings.get(i));
-				//System.out.println("expheading: "+expHeadings.get(i));
-				System.out.println(count + ": "+actionTable.findElements(By.tagName("th")).get(i).getText());
-				
-		}
 		int rowCount = actionTable.findElements(By.tagName("tr")).size();
-		System.out.println(rowCount);
-		//for(int i=1;i<rowCount;i++){
-		//		System.out.println(i + ": "+actionTable.findElements(By.tagName("tr")).get(i).getText());
-		//}
+		//System.out.println("Number of rows:"+rowCount);
+		//System.out.println("Number of headings:"+headingsCount);
+		List<String> expRow1 = Arrays.asList("Iuvaret","Apeirian","Adipisci","Definiebas","Consequuntur","Phaedrum"); 
+		
 		for(int i=1;i<rowCount;i++){
-			System.out.print("Row"+i+" ");
-			WebElement rowElement = actionTable.findElements(By.tagName("tr")).get(i);
-			for(int j=0;j<headingsCount;j++){
-				System.out.print(rowElement.findElements(By.tagName("td")).get(j).getText());
-				System.out.print(" ");
+			WebElement fullRow = actionTable.findElements(By.tagName("tr")).get(i);
+			for(int j=0;j<headingsCount-1;j++){
+				int index=i-1;
+				String actualRowElement = fullRow.findElements(By.tagName("td")).get(j).getText();
+				String expRowElement = expRow1.get(j)+index;
+				//System.out.println("actualRowElement:"+actualRowElement +" expRowElement:"+expRowElement);
+				Assert.assertEquals(actualRowElement,expRowElement);
 			}
-			System.out.println("");
 		}
+		
+	}
+	@Test(priority = 9)	
+	public void verifyEditDeleteLink() {
+		WebElement table = driver.findElement(By.cssSelector("div[class='large-10 columns']"));
+		WebElement actionTable = table.findElement(By.tagName("table"));
+		int headingsCount = actionTable.findElements(By.tagName("th")).size();
+		int rowCount = actionTable.findElements(By.tagName("tr")).size();
+		
 		//verify the edit and delete link
 		for(int i=1;i<rowCount;i++){
-			System.out.print("Row"+i+" ");
 			WebElement rowElement = actionTable.findElements(By.tagName("tr")).get(i);
 			System.out.println(rowElement.findElements(By.tagName("td")).get(headingsCount-1).getText());
 			rowElement.findElement(By.xpath("(//a[contains(@href,'edit')])")).click();
-			System.out.println(driver.getCurrentUrl());
+			String actualEditURL=driver.getCurrentUrl();
+			System.out.println(actualEditURL);
+			Assert.assertEquals(actualEditURL, editURL);
 			rowElement.findElement(By.xpath("(//a[contains(@href,'delete')])")).click();
-			System.out.println(driver.getCurrentUrl());
-		
+			String actualDeleteURL=driver.getCurrentUrl();
+			System.out.println(actualDeleteURL);
+			Assert.assertEquals(actualDeleteURL, deleteURL);	
+		}
 	}
-	
+	@Test(priority = 10)	
+	public void verifyForkImg() {
+		WebElement forkImg = driver.findElement(By.tagName("img"));
+		Assert.assertTrue(forkImg.isDisplayed());
+		forkImg.click();
+		String actualForkImgUrl= driver.getCurrentUrl();
+		System.out.println("After switching to Child window:"+actualForkImgUrl);
+		Assert.assertEquals(actualForkImgUrl, expForkImgURL);	
+		driver.navigate().back();
+	}
+
+	@Test(priority = 11)
+	public void verifyFooter() {
+		WebElement footerElement = driver.findElement(By.cssSelector("div[class='large-4 large-centered columns']"));
+		String footerText = driver.findElement(By.xpath("//div[text()='Powered by ']")).getText();
+		System.out.println("Footer Text:"+footerText);
+		Assert.assertEquals(footerText, expFooterText);
+		System.out.println("Footer: "+footerElement.findElement(By.xpath("//a[@href='http://elementalselenium.com/']")).getText());
+		Assert.assertTrue(driver.findElement(By.xpath("//a[@href='http://elementalselenium.com/']")).isDisplayed());
+		
+		footerElement.findElement(By.xpath("//a[@href='http://elementalselenium.com/']")).click();
+		
+		Set<String> windowIds = driver.getWindowHandles();
+			Iterator<String> windowIterator = windowIds.iterator();
+			String parentWindowId=windowIterator.next();
+			String childWindowId=windowIterator.next();
+			driver.switchTo().window(childWindowId);
+			String actualFootUrl= driver.getCurrentUrl();
+			System.out.println("After switching to Child window:"+actualFootUrl);
+			Assert.assertEquals(actualFootUrl, expFooterUrl);
+			
+			String actualFooterTitle = driver.getTitle();
+			System.out.println("After switching to Child window:"+actualFooterTitle);
+			Assert.assertEquals(actualFooterTitle, expFooterTitle);
+			
 	}
 
 @AfterSuite 
--- src/test/OpenBrowser.java
+++ src/test/OpenBrowser.java
@@ -3,15 +3,25 @@ package test;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.BeforeSuite;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
 
 public class OpenBrowser {
 	public WebDriver driver=null;
-	
+	String expFooterText = null;
+	String expFooterUrl =null;
+	String expFooterTitle =null;
+	String expForkImgURL=null;
+	String expectedTitle = null;
+	String expectedHeading = null;
+	String expectedDesc = null;
+	String editURL = null;
+	String deleteURL = null;
 	
 	public void launchBrowser() throws IOException {
 		Properties prop = new Properties();
@@ -20,11 +30,26 @@ public class OpenBrowser {
 		if(prop.getProperty("browser").equals("chrome")){
 			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Neethu\\work\\chrome\\version78\\chromedriver.exe");
 			driver =new ChromeDriver();
-			driver.get(prop.getProperty("url"));
-			driver.manage().window().maximize();
+			
 		}else if(prop.getProperty("browser").equals("firefox")){
-			driver =new FirefoxDriver();
+			System.setProperty("webdriver.gecko.driver","C:\\Users\\Neethu\\work\\firefox\\geckodriver.exe");
+			WebDriver driver= new FirefoxDriver();
+		}else {
+		System.setProperty("webdriver.ie.driver", "C:\\Users\\Neethu\\work\\ie\\MicrosoftWebDriver.exe");
+		WebDriver driver= new InternetExplorerDriver();
 		}
+		driver.get(prop.getProperty("url"));
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		expFooterText =prop.getProperty("expFooterText");
+		expFooterUrl =prop.getProperty("expFooterUrl");
+		expFooterTitle=prop.getProperty("expFooterTitle");
+		expForkImgURL=prop.getProperty("expForkImgURL");
+		expectedTitle=prop.getProperty("expectedTitle");
+		expectedHeading=prop.getProperty("expectedHeading");
+		expectedDesc = prop.getProperty("expectedDesc");
+		editURL = prop.getProperty("editURL");
+		deleteURL = prop.getProperty("deleteURL");
 	}
 	
 }
